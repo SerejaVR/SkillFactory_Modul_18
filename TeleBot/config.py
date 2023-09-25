153c29e98134ea7c160c1ecefb9878bfb5ce6c86
SerejaVR <rudenkov.sereja@yandex.ru>
2023-09-25 17:47:29 +0300

    task_23.6.1

---
+++ TeleBot/config.py
@@ -0,0 +1,12 @@
+TOKEN = "6350981016:AAEnf94Jt-wSqR6G1PrpNGZFgJImtBGqMIo"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    }
+
+
+
